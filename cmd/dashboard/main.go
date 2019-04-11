b34d406252915b8e6227dc291e0a157612bb2f18
jessica McCreery <jessica.mccreery@ibm.com>
2019-04-11 17:17:34 +0100

    remove pipeline0 code

--- cmd/dashboard/main.go
+++ cmd/dashboard/main.go
@@ -16,24 +16,16 @@ package main
 import (
 	"net/http"
 	"os"
-	"time"
 
 	restful "github.com/emicklei/go-restful"
 	endpoints "github.com/tektoncd/dashboard/pkg/endpoints"
 	logging "github.com/tektoncd/dashboard/pkg/logging"
 	clientset "github.com/tektoncd/pipeline/pkg/client/clientset/versioned"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	k8sclientset "k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/sample-controller/pkg/signals"
 )
-
-// Should be kicked off at install time;
-// the pipeline, repo and resource values here must match the values of the pipeline0 definition
-var pipeline0Name = "pipeline0"
-var pipeline0ResourceName = "git-source"
-var pipeline0RepoName = "tekton-pipeline-config"
 
 func main() {
 	var cfg *rest.Config
@@ -80,15 +72,8 @@ func main() {
 	logging.Log.Info("Registering REST endpoints")
 	resource.RegisterEndpoints(wsContainer)
 	resource.RegisterWebsocket(wsContainer)
-
-	namespace := os.Getenv("PIPELINE_RUN_NAMESPACE")
-
-	namespaceToUse := "default"
-	if namespace != "" {
-		namespaceToUse = namespace
-	}
-
-	createPipeline0(namespaceToUse, resource)
+	resource.RegisterHealthProbes(wsContainer)
+	resource.RegisterReadinessProbes(wsContainer)
 
 	stopCh := signals.SetupSignalHandler()
 	resource.StartPipelineRunController(stopCh)
@@ -96,72 +81,4 @@ func main() {
 	logging.Log.Infof("Creating server and entering wait loop")
 	server := &http.Server{Addr: port, Handler: wsContainer}
 	logging.Log.Fatal(server.ListenAndServe())
-}
-
-/* expected behaviour:
-- No PipelineRun exists already for pipeline0 at install time, create one (even on pod going away)
-- A PipelineRun exists already for pipeline0, don't create again */
-func createPipeline0(namespace string, resource endpoints.Resource) {
-	duration := 5 * time.Second
-	numAttempts := 4
-	count := 0
-	existingPipeline0Run := false
-
-	/* Poll every five seconds for twenty seconds: make sure we get the list of PipelineRuns to check for Pipeline0 runs */
-	for count < numAttempts {
-		pipelineRunList, appError := resource.GetAllPipelineRunsImpl(namespace, "")
-		if appError.ERROR != nil {
-			logging.Log.Errorf("there was a problem getting all PipelineRuns: %s", appError.ERROR)
-			time.Sleep(duration)
-		}
-
-		for _, pipelineRun := range pipelineRunList.Items {
-			if pipelineRun.Spec.PipelineRef.Name == pipeline0Name {
-				existingPipeline0Run = true
-				break
-			}
-		}
-		count++
-	}
-
-	if existingPipeline0Run == false {
-		pipeline0Created := false
-		numAttempts := 5
-		count := 0
-
-		/* Poll every sec for five seconds: make sure the Pipeline and Task are registered with Kube, may not be if we're too quick here */
-		for count < numAttempts {
-			taskExists, _ := resource.PipelineClient.TektonV1alpha1().Tasks(namespace).Get("pipeline0-task", metav1.GetOptions{})
-			pipeline0Exists, _ := resource.PipelineClient.TektonV1alpha1().Pipelines(namespace).Get("pipeline0", metav1.GetOptions{})
-			if taskExists.Name == "" && pipeline0Exists.Name == "" {
-				logging.Log.Debug("Didn't find pipeline and task")
-				time.Sleep(1 * time.Second)
-			} else {
-				pipeline0Created = true
-				break
-			}
-			count++
-		}
-
-		if !pipeline0Created {
-			logging.Log.Error("failed to apply Pipeline templates at install time")
-		}
-
-		pipelineTemplateRepo := os.Getenv("PIPELINE_TEMPLATE_REPO")
-		pipelineTemplateRepoRevision := os.Getenv("PIPELINE_TEMPLATE_REPO_REVISION")
-
-		pipeline0Run := endpoints.ManualPipelineRun{
-			// Pipeline0 vars are declared at the top of this file
-			PIPELINENAME:    pipeline0Name,
-			GITRESOURCENAME: pipeline0ResourceName,
-			GITCOMMIT:       pipelineTemplateRepoRevision,
-			REPOURL:         pipelineTemplateRepo,
-		}
-
-		createResponse := resource.CreatePipelineRunImpl(pipeline0Run, namespace)
-		if createResponse.ERROR != nil {
-			logging.Log.Errorf("a problem occured creating Pipeline0 PipelineRun: %s", createResponse.MESSAGE)
-		}
-	}
-
 }
