ce927961ba1e243068d06629402a394ffdf3e451
stephen <stephen.odonovancs@gmail.com>
2019-04-17 17:39:10 +0100

    update tests

--- src/components/CancelButton/CancelButton.test.js
+++ src/components/CancelButton/CancelButton.test.js
@@ -13,21 +13,32 @@ limitations under the License.
 
 import React from 'react';
 import { render } from 'react-testing-library';
-import { shallow, configure } from 'enzyme';
+import { configure, mount, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import CancelButton from './CancelButton';
 import { renderWithRouter } from '../../utils/test';
 
 configure({adapter: new Adapter()});
 
+describe('Test Button component submit', () => {
+  it('Test click event', () => {
+    const mockCallBack = jest.fn();
+
+    const button = mount((<CancelButton onCancel={mockCallBack}>Ok!</CancelButton>));
+    button.find('.cancel-button').simulate('click');
+    button.find('.bx--btn--primary').simulate('click');
+    expect(mockCallBack.mock.calls.length).toEqual(1);
+  });
+});
+
 describe('Test Button component', () => {
   it('Test click event', () => {
     const mockCallBack = jest.fn();
-    const type = "pipeline";
-    const name = "pipelineRun1";
 
-    const cancelButton = shallow((<CancelButton type={type} name={name} onSubmit={mockCallBack}>Ok!</CancelButton>));
-    cancelButton.find('button').simulate('click');
-    expect(queryByText(/pipelineRun1/i)).toBeTruthy();
+    const button = mount((<CancelButton onCancel={mockCallBack}>Ok!</CancelButton>));
+    button.find('.cancel-button').simulate('click');
+    button.find('.bx--btn--secondary').simulate('click');
+    expect(mockCallBack.mock.calls.length).toEqual(0);
   });
 });
+
