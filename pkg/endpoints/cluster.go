4cebc3a9771aae7cdddb5b9c87b7d2238b12b5c9
stephen <stephen.odonovancs@gmail.com>
2019-04-29 16:41:02 +0100

    add get namespaces api

---
+++ pkg/endpoints/cluster.go
@@ -0,0 +1,34 @@
+/*
+Copyright 2019 The Tekton Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+		http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package endpoints
+
+import (
+  "github.com/emicklei/go-restful"
+  "github.com/tektoncd/dashboard/pkg/logging"
+  "github.com/tektoncd/dashboard/pkg/utils"
+  "net/http"
+
+  metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func (r Resource) getAllNamespaces(request *restful.Request, response *restful.Response) {
+  namespaces, err := r.K8sClient.CoreV1().Namespaces().List(metav1.ListOptions{});
+  logging.Log.Debugf("In getAllNamespaces - : %s", namespaces)
+
+  if err != nil {
+    utils.RespondError(response, err, http.StatusNotFound)
+    return
+  }
+  response.WriteEntity(namespaces)
+}
