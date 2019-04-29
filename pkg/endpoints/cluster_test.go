56b077c04e2840334dc7b814e4c708da847f24a4
stephen <stephen.odonovancs@gmail.com>
2019-04-29 17:35:22 +0100

    fix indentation, update editor config rules

--- pkg/endpoints/cluster_test.go
+++ pkg/endpoints/cluster_test.go
@@ -14,36 +14,36 @@ limitations under the License.
 package endpoints
 
 import (
-  "encoding/json"
-  "net/http/httptest"
-  "testing"
+	"encoding/json"
+	"net/http/httptest"
+	"testing"
 
-  corev1 "k8s.io/api/core/v1"
-  metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func TestGetAllNamespaces(t *testing.T) {
 
-  r := dummyResource()
+	r := dummyResource()
 
-  namespace := "test-namespace"
-  r.K8sClient.CoreV1().Namespaces().Create(&corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: namespace}})
+	namespace := "test-namespace"
+	r.K8sClient.CoreV1().Namespaces().Create(&corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: namespace}})
 
-  // Test getAllPipelines function
-  httpReq := dummyHTTPRequest("GET", "http://wwww.dummy.com:8383/v1/namespaces/", nil)
-  req := dummyRestfulRequest(httpReq, "", "")
-  httpWriter := httptest.NewRecorder()
-  resp := dummyRestfulResponse(httpWriter)
+	// Test getAllPipelines function
+	httpReq := dummyHTTPRequest("GET", "http://wwww.dummy.com:8383/v1/namespaces/", nil)
+	req := dummyRestfulRequest(httpReq, "", "")
+	httpWriter := httptest.NewRecorder()
+	resp := dummyRestfulResponse(httpWriter)
 
-  r.getAllNamespaces(req, resp)
+	r.getAllNamespaces(req, resp)
 
-  result := corev1.NamespaceList{}
-  json.NewDecoder(httpWriter.Body).Decode(&result)
+	result := corev1.NamespaceList{}
+	json.NewDecoder(httpWriter.Body).Decode(&result)
 
-  if len(result.Items) != 1 {
-    t.Errorf("Number of namespaces: expected: %d, returned: %d", 1, len(result.Items))
-  }
-  if result.Items[0].Name != namespace {
-    t.Errorf("%s is not returned: %s, %s", namespace, result.Items[0].Name, result.Items[1].Name)
-  }
+	if len(result.Items) != 1 {
+		t.Errorf("Number of namespaces: expected: %d, returned: %d", 1, len(result.Items))
+	}
+	if result.Items[0].Name != namespace {
+		t.Errorf("%s is not returned: %s, %s", namespace, result.Items[0].Name, result.Items[1].Name)
+	}
 }
