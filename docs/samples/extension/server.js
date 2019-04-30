e575fbddc02253e68fc9185224dcfef3b3259858
Alan Greene <ALANGREE@ie.ibm.com>
2019-04-30 17:37:04 +0100

    Fix lint issues

--- docs/samples/extension/server.js
+++ docs/samples/extension/server.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const express = require('express');
 
 // Constants
@@ -9,11 +7,11 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-	res.send('Hello tekton dashboard!!\n');
-    });
+  res.send('Hello tekton dashboard!!\n');
+});
 app.get('/sample', (req, res) => {
-	res.send('Hello tekton dashboard!! Here is /sample\n');
-    });
+  res.send('Hello tekton dashboard!! Here is /sample\n');
+});
 
 app.listen(PORT, HOST);
-console.log(`Listening at  http://${HOST}:${PORT}`);
+console.log(`Listening at http://${HOST}:${PORT}`); // eslint-disable-line no-console
