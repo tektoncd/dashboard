3b27c97aa987419003b7f12eb4f9f8cb36c3809c
Carolyn Mabbott <Carolyn.Mabbott1@ibm.com>
2019-05-23 10:33:40 +0100

    Removed logging

--- test/e2e-tests.sh
+++ test/e2e-tests.sh
@@ -36,15 +36,15 @@ header "Running e2e tests"
 
 #kubectl cluster-info
 output=$(kubectl cluster-info)
-logging.Log.Debugf("kubectl cluster-info : $output")
+echo$("kubectl cluster-info : $output")
 
 #Gets the cluster info in the style: https://localhost:6443
 output1=$(kubectl cluster-info | head -n 1)
 #echo "kubectl cluster-info | head -n 1 :" $output1
-logging.Log.Debugf("kubectl cluster-info | head -n 1 : $output1")
+echo$("kubectl cluster-info | head -n 1 : $output1")
 edited=$(echo "$output1" | sed 's/.*running at \([^ ]*\).*/\1/')
 #echo "Edited is: "$edited
-logging.Log.Debugf("Edited is: $edited")
+echo$"Edited is: $edited")
 
 
 
