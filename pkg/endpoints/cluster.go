e5160888ae6b2b8cbd520d3b260eb1ae3e53bff2
CarolynMabbott <32342288+CarolynMabbott@users.noreply.github.com>
2019-05-28 13:27:13 +0100

    Remove debug comments

--- pkg/endpoints/cluster.go
+++ pkg/endpoints/cluster.go
@@ -24,13 +24,11 @@ import (
 
 func (r Resource) getAllNamespaces(request *restful.Request, response *restful.Response) {
 	namespaces, err := r.K8sClient.CoreV1().Namespaces().List(metav1.ListOptions{})
-	logging.Log.Debugf("In getAllNamespaces")
 
 	if err != nil {
 		utils.RespondError(response, err, http.StatusNotFound)
 		return
 	}
-	logging.Log.Debugf("Found namespaces: %s", namespaces)
 	response.WriteEntity(namespaces)
 }
 
@@ -38,12 +36,10 @@ func (r Resource) getAllServiceAccounts(request *restful.Request, response *rest
 	requestNamespace := utils.GetNamespace(request)
 
 	serviceAccounts, err := r.K8sClient.CoreV1().ServiceAccounts(requestNamespace).List(metav1.ListOptions{})
-	logging.Log.Debugf("In getAllServiceAccounts")
 
 	if err != nil {
 		utils.RespondError(response, err, http.StatusNotFound)
 		return
 	}
-	logging.Log.Debugf("Found service accounts: %s", serviceAccounts)
 	response.WriteEntity(serviceAccounts)
 }
