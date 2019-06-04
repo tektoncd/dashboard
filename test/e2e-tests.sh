7cfb90d48bca5bde52ee4489959f208bc1c87b64
Carolyn Mabbott <Carolyn.Mabbott1@ibm.com>
2019-06-04 16:40:32 +0100

    istio

--- test/e2e-tests.sh
+++ test/e2e-tests.sh
@@ -32,68 +32,11 @@ install_dashboard_backend
 
 # failed=0
 
-# # Run the integration tests
-# header "Running e2e tests"
+# Run the integration tests
+ header "Running e2e tests"
 # # TODO: run your test here !
 
-# function install_istio() {
-#     if [ -z "$1" ]; then
-#         echo "Usage ERROR for function: install_istio [version]"
-#         echo "Missing [version]"
-#         exit 1
-#     fi
-#     version="$1"
-#     # Install on Minikube or Docker Desktop
-#     kubectl apply --filename https://github.com/knative/serving/releases/download/${version}/istio-crds.yaml &&
-#     curl -L https://github.com/knative/serving/releases/download/${version}/istio.yaml \
-#       | kubectl apply --filename -
 
-#     # This works but why are we only labelling the default namespace? 
-#     # Isn't this needed on those namespaces in which we use knative-eventing?
-#     # If not is Istio really required for our purposes? 
-#     kubectl label namespace default istio-injection=enabled
-
-#     # Wait until all the pods come up
-#     wait_for_ready_pods istio-system 300 30
-# }
-
-# function install_knative_serving() {
-#     if [ -z "$1" ]; then
-#         echo "Usage ERROR for function: install_knative_serving [version]"
-#         echo "Missing [version]"
-#         exit 1
-#     fi
-#     version="$1"
-#     curl -L https://github.com/knative/serving/releases/download/${version}/serving.yaml \
-#     | kubectl apply --filename -
-#     # Wait until all the pods come up
-#     wait_for_ready_pods knative-serving 180 20
-# }
-
-# function install_knative_eventing() {
-#     if [ -z "$1" ]; then
-#         echo "Usage ERROR for function: install_knative_eventing [version]"
-#         echo "Missing [version]"
-#         exit 1
-#     fi
-#     version="$1"
-#     kubectl apply --filename https://github.com/knative/eventing/releases/download/${version}/release.yaml
-#     # Wait until all the pods come up
-#     wait_for_ready_pods knative-eventing 180 20
-# }
-
-# # Note that eventing-sources.yaml was renamed from release.yaml in the v0.5.0 release, so this won't work for earlier releases as-is. 
-# function install_knative_eventing_sources() {
-#     if [ -z "$1" ]; then
-#         echo "Usage ERROR for function: install_knative_eventing_sources [version]"
-#         echo "Missing [version]"
-#         exit 1
-#     fi
-#     version="$1"
-#     kubectl apply --filename https://github.com/knative/eventing-sources/releases/download/${version}/eventing-sources.yaml
-#     # Wait until all the pods come up
-#     wait_for_ready_pods knative-sources 180 20
-# }
 
 function wait_for_ready_pods() {
     if [ -z "$1" ] || [ -z "$2" ]; then
@@ -107,164 +50,18 @@ function wait_for_ready_pods() {
     timeout ${timeout_period} "kubectl get pods -n ${namespace} && [[ \$(kubectl get pods -n ${namespace} 2>&1 | grep -c -v -E '(Running|Completed|Terminating|STATUS)') -eq 0 ]]"
 }
 
-# # Output 0 if the kservice "Ready" condition is True, 1 otherwise
-# function kservice_is_ready() {
-#     if [ -z "$1" ] || [ -z "$2" ]; then
-#         echo "Usage ERROR for function: kservice_is_ready [kservice] [namespace]"
-#         [ -z "$1" ] && echo "Missing [kservice]"
-#         [ -z "$2" ] && echo "Missing [namespace]"
-#         exit 1
-#     fi
-#     kservice=$1
-#     namespace=$2
-
-#     kubectl get ksvc "$kservice" -n "$namespace" -o json \
-#         | jq '.status.conditions[] | select(.type == "Ready") | {status}' \
-#         | grep "True" > /dev/null \
-#         ; echo $?
-# }
-
-# function wait_for_ready_kservice() {
-#     if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
-#         echo "Usage ERROR for function: wait_for_ready_kservice [kservice] [namespace] [timeout] <sleepTime>"
-#         [ -z "$1" ] && echo "Missing [kservice]"
-#         [ -z "$2" ] && echo "Missing [namespace]"
-#         [ -z "$3" ] && echo "Missing [timeout]"
-#         exit 1
-#     fi
-#     kservice="$1"
-#     namespace="$2"
-#     timeout="$3"
-#     sleepTime=${4:-"2"}
-#     ctr=0
-#     until [ "0" = "$(kservice_is_ready "$kservice" "$namespace")" ]; do
-#         echo "waiting for ready $kservice kservice in namespace $namespace:"
-#         kubectl get ksvc "$kservice" --namespace $namespace
-#         if [ "$timeout" -ne "-1" ] && [ "$timeout" -le "$ctr" ]; then
-#             echo "ERROR: kservice $kservice was not ready in install namespace ${namespace} after waiting ${timeout} seconds."
-#             kubectl describe ksvc $kservice --namespace $namespace
-#             return 1
-#         fi
-#         sleep "$sleepTime"
-#         ctr=$((ctr+sleepTime))
-#     done
-#     kubectl get ksvc $kservice --namespace $namespace
-# }
-
-# function get_kserving_domain() {
-#     if [ -z "$1" ] || [ -z "$2" ]; then
-#         echo "Usage ERROR for function: get_kserving_domain [kservice] [namespace]"
-#         [ -z "$1" ] && echo "Missing [kservice]"
-#         [ -z "$2" ] && echo "Missing [namespace]"
-#         exit 1
-#     fi
-#     kservice="$1"
-#     namespace="$2"
-#     kubectl get ksvc "$kservice" -n "$namespace" -o json | jq '.status.domain' | sed 's/"//g'
-# }
-
-# function install_istio_nodeport() {
-#     if [ -z "$1" ]; then
-#         echo "Usage ERROR for function: install_istio_nodeport [version]"
-#         echo "Missing [version]"
-#         exit 1
-#     fi
-#     version="$1"
-#     # Install on Minikube
-#     # We are changing LoadBalancer to NodePort for the istio-ingress service)
-#     kubectl apply --filename https://github.com/knative/serving/releases/download/${version}/istio-crds.yaml &&
-#     curl -L https://github.com/knative/serving/releases/download/${version}/istio.yaml \
-#     | sed 's/LoadBalancer/NodePort/' \
-#     | kubectl apply --filename -
-#     # Label the default namespace with istio-injection=enabled.
-#     kubectl label namespace default istio-injection=enabled
-#     # Wait until all the pods come up
-#     wait_for_ready_pods istio-system 300 30
-# }
-
-# function install_knative_serving_nodeport() {
-#     if [ -z "$1" ]; then
-#         echo "Usage ERROR for function: install_knative_serving_nodeport [version]"
-#         echo "Missing [version]"
-#         exit 1
-#     fi
-#     version="$1"
-#     # Use NodePort instead of LoadBalancer for Minikube
-#     curl -L https://github.com/knative/serving/releases/download/${version}/serving.yaml \
-#     | sed 's/LoadBalancer/NodePort/' \
-#     | kubectl apply --filename -
-#     # Wait until all the pods come up
-#     wait_for_ready_pods knative-serving 180 20
-# }
-
-
-# #echo "Installing knative version $KNATIVE_VERSION"
-# install_istio_nodeport $KNATIVE_VERSION
-# #install_istio $KNATIVE_VERSION
-# install_knative_serving $KNATIVE_VERSION
-# install_knative_eventing $KNATIVE_VERSION
-# install_knative_eventing_sources $KNATIVE_VERSION
-# install_knative_serving_nodeport $KNATIVE_VERSION
-# echo "Installed knative version $KNATIVE_VERSION"
-
-#echo "Applying test-rbac,yaml"
-#kubectl apply -f $tekton_repo_dir/test/test-rbac.yaml
-#echo "Applied test-rbac.yaml"
-
-#kubectl cluster-info
-#output=$(kubectl cluster-info)
-#echo "kubectl cluster-info : $output"
-
-#Gets the cluster info in the style: https://localhost:6443
-#output1=$(kubectl cluster-info | head -n 1)
-#echo "kubectl cluster-info | head -n 1 : $output1"
-#edited=$(echo "$output1" | sed 's/.*://') #sed 's/.*running at \([^ ]*\).*/\1/')
-#edited1=$(echo "$output1" | sed 's/.*https://\([^ ]*\).*/\1/')
-#echo "Edited1 is $edited1"
-#edited=$(echo "$edited1 | sed 's/ .*//'") #cut -f1 -d" "")
-#echo "Edited is: $edited"
-
-# edited1=$(echo "$edited" | sed 's/.\{4\}$//')  
-# #| sed s'/[a-zA-Z]$//') #sed 's/.*https: \([^ ]*\).*/\1/')
-# echo "Edited1 is: $edited1"
-
-# ip="https:$edited1"
-# echo "ip is $ip"
-
-#edited2=$(echo "$edited" | sed 's/ .*//')
-#echo "Edited2 is: $edited2"
 
 kubectl port-forward $(kubectl get pod -l app=tekton-dashboard -o name) 9097:9097 &
 #port_forward &
 echo "dashboard forwarded to port 9097"
 
 
-# pods=$(kubectl get pods)
-# echo "Pods running are: $pods"
 
-# deployments=$(kubectl get deployments)
-# echo "Deployments running are: $deployments"
-
-# pipeline=$(kubectl get pipeline)
-# echo "pipeline running are: $pipeline"
-
-# pipelineresource=$(kubectl get pipelineresource)
-# echo "pipelineresource running are: $pipelineresource"
-
-# pipelinerun=$(kubectl get pipelinerun)
-# echo "pipelinerun running are: $pipelinerun"
-
-# tasks=$(kubectl get tasks)
-# echo "tasks running are: $tasks"
-
-# echo "$tekton_repo_dir"
 
 kubectl apply -f $tekton_repo_dir/test/Task.yaml
 echo "kubectl apply -f tekton_repo_dir/test/Task.yaml"
 
-# tasks=$(kubectl get tasks)
-# echo "tasks running are: "
-# echo "$tasks"
+
 
 kubectl apply -f $tekton_repo_dir/test/Pipeline.yaml
 echo "kubectl apply -f tekton_repo_dir/test/Pipeline.yaml"
@@ -275,94 +72,8 @@ echo "kubectl apply -f tekton_repo_dir/test/PipelineRun.yaml"
 
 wait_until_pods_running default
 
-# pods=$(kubectl get pods)
-# echo "Pods running are: "
-# echo "$pods"
-
-# deployments=$(kubectl get deployments)
-# echo "Deployments running are: "
-# echo "$deployments"
-
-# ksvc=$(kubectl get ksvc)
-# echo "KSVC are:"
-# echo "$ksvc"
-
-# ssvc=$(kubectl get svc)
-# echo "SVC are:"
-# echo "$svc"
-
-# curling=$(curl -k $ip)
-# echo "ip =$ip"
-# echo "curling"
-
-# echo "response pipelne is : $ip:9097/v1/namespaces/default/pipelines"
-
-#responsePipeline=$(curl -k "$ip:9097/v1/namespaces/default/pipelines")
-#echo "response is :"
-#echo "$responsePipeline"  
-
-#responsePipelineRun=$(curl -k "$ip:9097/v1/namespaces/default/pipelineruns")
-#echo "response is :"
-#echo "$responsePipelineRun"  
-
-#responseTask=$(curl -k "$ip:9097/v1/namespaces/default/tasks")
-#echo "response is :"
-#echo "$responseTask" 
 
 
-
-# namespaces=$(kubectl get namespaces)
-# echo "namespaces are:"
-# echo "$namespaces"
-
-# responseLocal=$(curl -k "127.0.0.1:9097/v1/namespaces/default/pipelines")
-# echo "response is :"
-# echo "$responseLocal" 
-
-# responseLocalName=$($responseLocal | jq -r 'name')
-# echo "responseLocalName is : $responseLocalName"
-
-# responseLocal1=$(curl -k "127.0.0.1:9097/v1/namespaces/default/pipelineruns")
-# echo "response is :"
-# echo "$responseLocal1"  
-
-# responseLocal2=$(curl -k "127.0.0.1:9097/v1/namespaces/default/tasks")
-# echo "response is :"
-# echo "$responseLocal2" 
-
-
-# ###FIX THIS - Istio pods not always coing up in time 
-# sleep 1m
-# # Wait until all the pods come up
-# wait_for_ready_pods istio-system 10000 300
-
-
-# istios=$(kubectl get pods -n istio-system)
-# echo "istio-system pods are"
-# echo "$istios"
-
-
-# ##How to get logs without numbers at the end 
-# #logs=$(kubectl logs -l app=istio-egressgateway -n istio-system)
-# #echo "logs is: $logs"
-
-# ksvc=$(kubectl get ksvc)
-# echo "KSVC are:"
-# echo "$ksvc"
-
-svc=$(kubectl get svc)
-echo "SVC are:"
-echo "$svc"
-
-# svcT=$(kubectl describe svc tekton-dashboard)
-# echo "svcT is $svcT"
-
-# svcIstio=$(kubectl get svc -n istio-system)
-# echo "svc isito are:"
-# echo "$svcIstio"
-
-#nport=$(kubectl get svc "istio-ingressgateway" --namespace istio-system --output 'jsonpath={.spec.ports[?(@.port==80)].nodePort}')
-#echo "nport is $nport"
 
 #API configuration
 APP_NS="default"
@@ -432,6 +143,8 @@ echo "Curled"
 # echo "Curling original"
 # curl -H "Host: ${domain}" -X POST --header Content-Type:application/json -d "$post_data" $curlNport/v1/namespaces/default/pipelinerun/
 # echo "Curled"
+
+
 
 wait_for_ready_kservice "$REPO_NAME" "$APP_NS" 500 10
 
