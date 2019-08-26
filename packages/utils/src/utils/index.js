18addcff5e330b3bea1ee5ee1318aa6f9b38d2e6
stephen <stephen.odonovancs@gmail.com>
2019-08-26 16:45:55 +0100

    Add pipeline Run to standalone components

--- packages/utils/src/utils/index.js
+++ packages/utils/src/utils/index.js
@@ -30,6 +30,82 @@ export function getErrorMessage(error) {
   );
 }
 
+export function selectedTask(selectedTaskName, tasks) {
+  return tasks.find(t => t.metadata.name === selectedTaskName);
+}
+
+export function selectedTaskRun(selectedTaskId, taskRuns = []) {
+  return taskRuns.find(run => run.id === selectedTaskId);
+}
+
+export function taskRunStep(selectedStepId, taskRun) {
+  if (!taskRun || !taskRun.steps) {
+    return {};
+  }
+  const step = taskRun.steps.find(s => s.id === selectedStepId);
+  if (!step) {
+    return {};
+  }
+
+  const { stepName, stepStatus, status, reason, ...definition } = step;
+
+  return {
+    definition,
+    reason,
+    stepName,
+    stepStatus,
+    status
+  };
+}
+
+export function stepsStatus(taskSteps, taskRunStepsStatus = []) {
+  const steps = taskSteps.map(step => {
+    const stepStatus =
+      taskRunStepsStatus.find(status => status.name === step.name) || {};
+
+    let status;
+    let reason;
+    if (stepStatus.terminated) {
+      status = 'terminated';
+      ({ reason } = stepStatus.terminated);
+    } else if (stepStatus.running) {
+      status = 'running';
+    } else if (stepStatus.waiting) {
+      status = 'waiting';
+    }
+
+    return {
+      ...step,
+      reason,
+      status,
+      stepStatus,
+      stepName: step.name,
+      id: step.name
+    };
+  });
+
+  /*
+    In case of failure in an init step (git-source, init-creds, etc.),
+    include that step in the displayed list so we can surface status
+    and logs to aid the user in debugging.
+   */
+  taskRunStepsStatus.forEach(stepStatus => {
+    const { name: stepName, terminated } = stepStatus;
+    const step = taskSteps.find(taskStep => taskStep.name === stepName);
+    if (!step && terminated && terminated.exitCode !== 0) {
+      steps.push({
+        reason: terminated.reason,
+        status: 'terminated',
+        stepStatus,
+        stepName,
+        id: stepName
+      });
+    }
+  });
+
+  return steps;
+}
+
 export function getStatus(resource) {
   const { conditions = [] } = resource.status || {};
   return conditions.find(condition => condition.type === 'Succeeded') || {};
