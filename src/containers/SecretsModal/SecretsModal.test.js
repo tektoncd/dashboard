85029d053ecfb933d9a83466f935aded1d498b82
Alan Greene <ALANGREE@ie.ibm.com>
2020-01-06 21:27:39 +0000

    Fix lint warnings

--- src/containers/SecretsModal/SecretsModal.test.js
+++ src/containers/SecretsModal/SecretsModal.test.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2019 The Tekton Authors
+Copyright 2019-2020 The Tekton Authors
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -19,9 +19,6 @@ import thunk from 'redux-thunk';
 import { renderWithIntl, rerenderWithIntl } from '../../utils/test';
 import SecretsModal from '.';
 import * as API from '../../api';
-
-// Declares scrollIntoView as a function for testing purposes
-window.HTMLElement.prototype.scrollIntoView = function scrollIntoViewTestStub() {};
 
 const middleware = [thunk];
 const mockStore = configureStore(middleware);
