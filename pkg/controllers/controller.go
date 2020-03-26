fec8e1d86e7bcc12f907face15b067aa62c68d55
Alan Greene <ALANGREE@ie.ibm.com>
2020-03-26 02:19:07 +0000

    Add websocket events for ServiceAccounts
    Update our websocket broadcaster to send ServiceAccount
    created / updated / deleted events so we can keep the UI
    in sync with the state of the cluster.

--- pkg/controllers/controller.go
+++ pkg/controllers/controller.go
@@ -1,3 +1,16 @@
+/*
+Copyright 2019-2020 The Tekton Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+		http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package controllers
 
 import (
@@ -41,6 +54,7 @@ func StartKubeControllers(clientset k8sclientset.Interface, resyncDur time.Durat
 	kubecontroller.NewExtensionController(kubeInformerFactory, installNamespace, handler)
 	kubecontroller.NewNamespaceController(kubeInformerFactory)
 	kubecontroller.NewSecretController(kubeInformerFactory)
+	kubecontroller.NewServiceAccountController(kubeInformerFactory)
 	// Started once all controllers have been registered
 	logging.Log.Info("Starting Kube controllers")
 	kubeInformerFactory.Start(stopCh)
