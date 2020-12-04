50e90ed0eaeeb138050961a2031b1a609f5a4524
Alan Greene <ALANGREE@ie.ibm.com>
2020-12-04 21:22:45 +0000

    Update to webpack 5
    Update webpack to v5. This also requires updating all of the
    supporting loaders / plugins since there were a number of breaking
    changes in events and configuration.
    
    This produces minimal reduction in chunk sizes, which is expected,
    as I've kept the configuration as close as possible to what we were
    using with webpack 4 so we can identify any new issues before making
    further changes to optimise our config.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2019 The Tekton Authors
+Copyright 2019-2020 The Tekton Authors
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -10,10 +10,10 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const path = require('path');
 const webpack = require('webpack');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const common = require('./webpack.common.js');
@@ -43,17 +43,13 @@ module.exports = merge(common({ mode }), {
     rules: [
       {
         test: /\.scss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          { loader: 'css-loader', options: { sourceMap: true } },
-          { loader: 'sass-loader', options: { sourceMap: true } }
-        ]
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
       }
     ]
   },
   plugins: [
     new CleanWebpackPlugin(),
-    new webpack.HashedModuleIdsPlugin(),
+    new webpack.ids.HashedModuleIdsPlugin(),
     new MiniCssExtractPlugin({
       filename: '[name].[contenthash].css'
     })
