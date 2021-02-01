b4355a0af04250fe9cc1586ea2e12320f19983cc
Alan Greene <ALANGREE@ie.ibm.com>
2021-02-01 11:10:52 +0000

    Remove PluralRules polyfill
    `Intl.PluralRules` is available natively in all supported browsers.
    Remove the polyfill as it's no longer required.

--- src/utils/polyfills.js
+++ src/utils/polyfills.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2019-2020 The Tekton Authors
+Copyright 2019-2021 The Tekton Authors
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -14,6 +14,5 @@ limitations under the License.
 
 import 'core-js/stable';
 
-import '@formatjs/intl-pluralrules/polyfill';
 import '@formatjs/intl-relativetimeformat/polyfill';
 import '@formatjs/intl-relativetimeformat/locale-data/en';
