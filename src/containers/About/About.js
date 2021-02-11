9e68fe5dce46e00981f062af562703682e351b4d
Alan Greene <ALANGREE@ie.ibm.com>
2021-02-11 13:17:07 +0000

    Use new Dashboard logo on the about page

--- src/containers/About/About.js
+++ src/containers/About/About.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2020 The Tekton Authors
+Copyright 2020-2021 The Tekton Authors
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -18,7 +18,7 @@ import { InlineNotification } from 'carbon-components-react';
 import { Table } from '@tektoncd/dashboard-components';
 import { getErrorMessage, getTitle } from '@tektoncd/dashboard-utils';
 
-import tektonLogo from '../../images/tekton-stacked-color.svg';
+import tektonLogo from '../../images/tekton-dashboard-color.svg';
 
 import {
   getDashboardNamespace,
