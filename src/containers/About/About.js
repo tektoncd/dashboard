5455209b65fdbb0ebac13e10106c5e542a1fa83e
Alan Greene <github.com@alangreene.net>
2021-03-14 20:53:13 +0000

    Refactor containers to use hooks
    Refactor the App, About, Conditions, and Condition containers to
    be functional components using React hooks instead of being class
    components.
    
    This change is a prerequisite for adopting react-query and other
    libraries which provide a hooks-based API.

--- src/containers/About/About.js
+++ src/containers/About/About.js
@@ -11,7 +11,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { injectIntl } from 'react-intl';
 import { InlineNotification } from 'carbon-components-react';
@@ -28,27 +28,37 @@ import {
   getPipelineVersion,
   getTriggersNamespace,
   getTriggersVersion,
-  isOpenShift,
-  isReadOnly,
-  isTriggersInstalled
+  isOpenShift as selectIsOpenShift,
+  isReadOnly as selectIsReadOnly,
+  isTriggersInstalled as selectIsTriggersInstalled
 } from '../../reducers';
 
 import './About.scss';
 
-export /* istanbul ignore next */ class About extends Component {
-  componentDidMount() {
-    const { intl } = this.props;
+/* istanbul ignore next */
+export function About({
+  dashboardNamespace,
+  dashboardVersion,
+  intl,
+  isOpenShift,
+  isReadOnly,
+  isTriggersInstalled,
+  logoutURL,
+  pipelinesNamespace,
+  pipelinesVersion,
+  triggersNamespace,
+  triggersVersion
+}) {
+  useEffect(() => {
     document.title = getTitle({
       page: intl.formatMessage({
         id: 'dashboard.about.title',
         defaultMessage: 'About'
       })
     });
-  }
+  }, []);
 
-  getDisplayValue = value => {
-    const { intl } = this.props;
-
+  const getDisplayValue = value => {
     switch (value) {
       case true:
         return intl.formatMessage({
@@ -60,13 +70,12 @@ export /* istanbul ignore next */ class About extends Component {
     }
   };
 
-  checkMissingProperties = () => {
-    const { intl } = this.props;
+  const checkMissingProperties = () => {
     const propertiesToCheck = {
-      DashboardNamespace: this.props.dashboardNamespace,
-      DashboardVersion: this.props.dashboardVersion,
-      PipelineNamespace: this.props.pipelinesNamespace,
-      PipelineVersion: this.props.pipelinesVersion
+      DashboardNamespace: dashboardNamespace,
+      DashboardVersion: dashboardVersion,
+      PipelineNamespace: pipelinesNamespace,
+      PipelineVersion: pipelinesVersion
     };
 
     const errorsFound = Object.keys(propertiesToCheck)
@@ -84,143 +93,139 @@ export /* istanbul ignore next */ class About extends Component {
       : null;
   };
 
-  render() {
-    const { intl } = this.props;
+  const headers = [
+    {
+      key: 'property',
+      header: intl.formatMessage({
+        id: 'dashboard.tableHeader.property',
+        defaultMessage: 'Property'
+      })
+    },
+    {
+      key: 'value',
+      header: intl.formatMessage({
+        id: 'dashboard.tableHeader.value',
+        defaultMessage: 'Value'
+      })
+    }
+  ];
 
-    const headers = [
-      {
-        key: 'property',
-        header: intl.formatMessage({
-          id: 'dashboard.tableHeader.property',
-          defaultMessage: 'Property'
-        })
-      },
-      {
-        key: 'value',
-        header: intl.formatMessage({
-          id: 'dashboard.tableHeader.value',
-          defaultMessage: 'Value'
-        })
-      }
-    ];
-
-    const getRow = (property, value) => {
-      const displayValue = this.getDisplayValue(value);
-      return displayValue
-        ? {
-            id: property,
-            property,
-            value: displayValue
-          }
-        : undefined;
-    };
-
-    const error = this.checkMissingProperties();
-
-    const versionLabel = intl.formatMessage({
-      id: 'dashboard.about.version',
-      defaultMessage: 'Version'
-    });
-    const isOpenShiftLabel = intl.formatMessage({
-      id: 'dashboard.about.isOpenShift',
-      defaultMessage: 'IsOpenShift'
-    });
-    const isReadOnlyLabel = intl.formatMessage({
-      id: 'dashboard.about.isReadOnly',
-      defaultMessage: 'ReadOnly'
-    });
-    const logoutURLLabel = intl.formatMessage({
-      id: 'dashboard.about.logoutURL',
-      defaultMessage: 'LogoutURL'
-    });
-
+  const getRow = (property, value) => {
+    const displayValue = getDisplayValue(value);
     return (
-      <div className="tkn--about">
-        <h1>
-          {intl.formatMessage({
-            id: 'dashboard.about.title',
-            defaultMessage: 'About'
+      displayValue && {
+        id: property,
+        property,
+        value: displayValue
+      }
+    );
+  };
+
+  const error = checkMissingProperties();
+
+  const versionLabel = intl.formatMessage({
+    id: 'dashboard.about.version',
+    defaultMessage: 'Version'
+  });
+  const isOpenShiftLabel = intl.formatMessage({
+    id: 'dashboard.about.isOpenShift',
+    defaultMessage: 'IsOpenShift'
+  });
+  const isReadOnlyLabel = intl.formatMessage({
+    id: 'dashboard.about.isReadOnly',
+    defaultMessage: 'ReadOnly'
+  });
+  const logoutURLLabel = intl.formatMessage({
+    id: 'dashboard.about.logoutURL',
+    defaultMessage: 'LogoutURL'
+  });
+
+  return (
+    <div className="tkn--about">
+      <h1>
+        {intl.formatMessage({
+          id: 'dashboard.about.title',
+          defaultMessage: 'About'
+        })}
+      </h1>
+      {error && (
+        <InlineNotification
+          kind="error"
+          title={intl.formatMessage({
+            id: 'dashboard.about.error',
+            defaultMessage: 'Error getting data'
           })}
-        </h1>
-        {error && (
-          <InlineNotification
-            kind="error"
-            title={intl.formatMessage({
-              id: 'dashboard.about.error',
-              defaultMessage: 'Error getting data'
-            })}
-            subtitle={getErrorMessage(error)}
-            lowContrast
+          subtitle={getErrorMessage(error)}
+          lowContrast
+        />
+      )}
+      <div className="tkn--about--content">
+        <div className="tkn--about--tables">
+          <Table
+            id="tkn--about--dashboard-table"
+            headers={headers}
+            rows={[
+              getRow('Namespace', dashboardNamespace),
+              getRow(versionLabel, dashboardVersion),
+              getRow(isOpenShiftLabel, isOpenShift),
+              getRow(isReadOnlyLabel, isReadOnly),
+              getRow(logoutURLLabel, logoutURL)
+            ].filter(Boolean)}
+            size="short"
+            title="Dashboard"
           />
-        )}
-        <div className="tkn--about--content">
-          <div className="tkn--about--tables">
-            <Table
-              id="tkn--about--dashboard-table"
-              headers={headers}
-              rows={[
-                getRow('Namespace', this.props.dashboardNamespace),
-                getRow(versionLabel, this.props.dashboardVersion),
-                getRow(isOpenShiftLabel, this.props.isOpenShift),
-                getRow(isReadOnlyLabel, this.props.isReadOnly),
-                getRow(logoutURLLabel, this.props.logoutURL)
-              ].filter(Boolean)}
-              size="short"
-              title="Dashboard"
-            />
+          <Table
+            headers={headers}
+            id="tkn--about--pipelines-table"
+            rows={[
+              getRow('Namespace', pipelinesNamespace),
+              getRow(versionLabel, pipelinesVersion)
+            ].filter(Boolean)}
+            size="short"
+            title="Pipelines"
+          />
+          {isTriggersInstalled && (
             <Table
               headers={headers}
-              id="tkn--about--pipelines-table"
+              id="tkn--about--triggers-table"
               rows={[
-                getRow('Namespace', this.props.pipelinesNamespace),
-                getRow(versionLabel, this.props.pipelinesVersion)
+                getRow('Namespace', triggersNamespace),
+                getRow(versionLabel, triggersVersion)
               ].filter(Boolean)}
               size="short"
-              title="Pipelines"
+              title="Triggers"
             />
-            {this.props.isTriggersInstalled && (
-              <Table
-                headers={headers}
-                id="tkn--about--triggers-table"
-                rows={[
-                  getRow('Namespace', this.props.triggersNamespace),
-                  getRow(versionLabel, this.props.triggersVersion)
-                ].filter(Boolean)}
-                size="short"
-                title="Triggers"
-              />
-            )}
-          </div>
-          <div className="tkn--about--image-wrapper">
-            <img
-              alt={intl.formatMessage({
-                id: 'dashboard.logo.alt',
-                defaultMessage: 'Tekton logo'
-              })}
-              role="presentation"
-              src={tektonLogo}
-              title={intl.formatMessage({
-                id: 'dashboard.logo.tooltip',
-                defaultMessage: 'Meow'
-              })}
-            />
-          </div>
+          )}
+        </div>
+        <div className="tkn--about--image-wrapper">
+          <img
+            alt={intl.formatMessage({
+              id: 'dashboard.logo.alt',
+              defaultMessage: 'Tekton logo'
+            })}
+            role="presentation"
+            src={tektonLogo}
+            title={intl.formatMessage({
+              id: 'dashboard.logo.tooltip',
+              defaultMessage: 'Meow'
+            })}
+          />
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 /* istanbul ignore next */
 const mapStateToProps = state => ({
   dashboardNamespace: getDashboardNamespace(state),
   dashboardVersion: getDashboardVersion(state),
-  isReadOnly: isReadOnly(state),
-  isOpenShift: isOpenShift(state),
+  isOpenShift: selectIsOpenShift(state),
+  isReadOnly: selectIsReadOnly(state),
+  isTriggersInstalled: selectIsTriggersInstalled(state),
   logoutURL: getLogoutURL(state),
   pipelinesNamespace: getPipelineNamespace(state),
   pipelinesVersion: getPipelineVersion(state),
-  isTriggersInstalled: isTriggersInstalled(state),
   triggersNamespace: getTriggersNamespace(state),
   triggersVersion: getTriggersVersion(state)
 });
