6d140d8f3019d2cbc27ee34387b90f2dab399246
Alan Greene <github.com@alangreene.net>
2021-05-14 11:18:52 +0100

    Refactor TaskRun to functional component

--- src/containers/TaskRun/TaskRun.js
+++ src/containers/TaskRun/TaskRun.js
@@ -11,7 +11,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import React, { Component } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { injectIntl } from 'react-intl';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
@@ -32,7 +32,8 @@ import {
   getStepStatus,
   getTitle,
   queryParams as queryParamConstants,
-  urls
+  urls,
+  useWebSocketReconnected
 } from '@tektoncd/dashboard-utils';
 
 import {
@@ -47,98 +48,108 @@ import {
   getTaskByType,
   getTaskRun,
   getTaskRunsErrorMessage,
-  isReadOnly,
   isWebSocketConnected,
-  isLogStreamingEnabled as selectIsLogStreamingEnabled
+  isLogStreamingEnabled as selectIsLogStreamingEnabled,
+  isReadOnly as selectIsReadOnly
 } from '../../reducers';
 
-import { fetchTask, fetchTaskByType } from '../../actions/tasks';
-import { fetchTaskRun } from '../../actions/taskRuns';
+import {
+  fetchTask as fetchTaskActionCreator,
+  fetchTaskByType as fetchTaskByTypeActionCreator
+} from '../../actions/tasks';
+import { fetchTaskRun as fetchTaskRunActionCreator } from '../../actions/taskRuns';
 import { rerunTaskRun } from '../../api';
 
 const taskTypeKeys = { ClusterTask: 'clustertasks', Task: 'tasks' };
 const { STEP, TASK_RUN_DETAILS, VIEW } = queryParamConstants;
 
-export /* istanbul ignore next */ class TaskRunContainer extends Component {
-  // once redux store is available errors will be handled properly with dedicated components
-  static notification({ intl, kind, message }) {
-    const titles = {
-      info: intl.formatMessage({
-        id: 'dashboard.taskRun.unavailable',
-        defaultMessage: 'TaskRun not available'
-      }),
-      error: intl.formatMessage({
-        id: 'dashboard.taskRun.errorLoading',
-        defaultMessage: 'Error loading TaskRun'
-      })
-    };
-    return (
-      <InlineNotification
-        kind={kind}
-        hideCloseButton
-        lowContrast
-        title={titles[kind]}
-        subtitle={message}
-      />
-    );
-  }
+function notification({ intl, kind, message }) {
+  const titles = {
+    info: intl.formatMessage({
+      id: 'dashboard.taskRun.unavailable',
+      defaultMessage: 'TaskRun not available'
+    }),
+    error: intl.formatMessage({
+      id: 'dashboard.taskRun.errorLoading',
+      defaultMessage: 'Error loading TaskRun'
+    })
+  };
+  return (
+    <InlineNotification
+      kind={kind}
+      hideCloseButton
+      lowContrast
+      title={titles[kind]}
+      subtitle={message}
+    />
+  );
+}
 
-  constructor(props) {
-    super(props);
+export /* istanbul ignore next */ function TaskRunContainer(props) {
+  const {
+    error,
+    externalLogsURL,
+    fetchTaskByType,
+    fetchTaskRun,
+    history,
+    intl,
+    isLogStreamingEnabled,
+    isReadOnly,
+    location,
+    match,
+    namespace,
+    selectedStepId,
+    showTaskRunDetails,
+    task,
+    taskRun,
+    view,
+    webSocketConnected
+  } = props;
 
-    this.state = {
-      isLogsMaximized: false,
-      loading: true,
-      showRerunNotification: null
-    };
-    this.maximizedLogsContainer = React.createRef();
-  }
+  const { taskRunName } = match.params;
 
-  componentDidMount() {
-    const { match, namespace } = this.props;
-    const { taskRunName } = match.params;
+  const maximizedLogsContainer = useRef();
+  const [isLogsMaximized, setIsLogsMaximized] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [rerunNotification, setRerunNotification] = useState(null);
+
+  useEffect(() => {
     document.title = getTitle({
       page: 'TaskRun',
       resourceName: taskRunName
     });
-    this.fetchTaskAndRuns(taskRunName, namespace);
+  }, []);
+
+  function fetchTaskAndRuns() {
+    fetchTaskRun({ name: taskRunName, namespace }).then(run => {
+      if (run && run.spec.taskRef) {
+        const { name, kind } = run.spec.taskRef;
+        fetchTaskByType(name, taskTypeKeys[kind], namespace).then(() => {
+          setLoading(false);
+        });
+      } else {
+        setLoading(false);
+      }
+    });
   }
 
-  componentDidUpdate(prevProps) {
-    const { match, namespace, webSocketConnected } = this.props;
-    const { taskRunName } = match.params;
-    const {
-      match: prevMatch,
-      namespace: prevNamespace,
-      webSocketConnected: prevWebSocketConnected
-    } = prevProps;
-    const { taskRunName: prevTaskRunName } = prevMatch.params;
+  useEffect(() => {
+    fetchTaskAndRuns(taskRunName, namespace);
+  }, [namespace, taskRunName]);
 
-    const websocketReconnected =
-      webSocketConnected && prevWebSocketConnected === false;
+  useWebSocketReconnected(
+    () => fetchTaskAndRuns(taskRunName, namespace),
+    webSocketConnected
+  );
 
-    if (
-      taskRunName !== prevTaskRunName ||
-      namespace !== prevNamespace ||
-      websocketReconnected
-    ) {
-      this.setState({ loading: !websocketReconnected }); // eslint-disable-line
-      this.fetchTaskAndRuns(taskRunName, namespace);
-    }
+  function toggleLogsMaximized() {
+    setIsLogsMaximized(state => !state);
   }
 
-  getLogContainer({ stepName, stepStatus, taskRun }) {
-    const {
-      externalLogsURL,
-      isLogStreamingEnabled,
-      selectedStepId
-    } = this.props;
-
+  function getLogContainer({ stepName, stepStatus, taskRun: run }) {
     if (!selectedStepId || !stepStatus) {
       return null;
     }
-
-    const { isLogsMaximized } = this.state;
 
     const logsRetriever = getLogsRetriever(
       isLogStreamingEnabled,
@@ -150,18 +161,17 @@ export /* istanbul ignore next */ class TaskRunContainer extends Component {
     return (
       <LogsRoot
         {...(isLogsMaximized
-          ? { container: this.maximizedLogsContainer.current }
+          ? { container: maximizedLogsContainer.current }
           : null)}
       >
         <Log
           toolbar={getLogsToolbar({
             isMaximized: isLogsMaximized,
             stepStatus,
-            taskRun,
-            toggleMaximized:
-              !!this.maximizedLogsContainer && this.toggleLogsMaximized
+            taskRun: run,
+            toggleMaximized: !!maximizedLogsContainer && toggleLogsMaximized
           })}
-          fetchLogs={() => logsRetriever(stepName, stepStatus, taskRun)}
+          fetchLogs={() => logsRetriever(stepName, stepStatus, run)}
           key={stepName}
           stepStatus={stepStatus}
         />
@@ -169,196 +179,156 @@ export /* istanbul ignore next */ class TaskRunContainer extends Component {
     );
   }
 
-  toggleLogsMaximized = () => {
-    this.setState(({ isLogsMaximized }) => ({
-      isLogsMaximized: !isLogsMaximized
-    }));
-  };
-
-  handleTaskSelected = (_, selectedStepId) => {
-    const { history, location, match } = this.props;
+  function handleTaskSelected(_, newSelectedStepId) {
     const queryParams = new URLSearchParams(location.search);
 
-    if (selectedStepId) {
-      queryParams.set(STEP, selectedStepId);
+    if (newSelectedStepId) {
+      queryParams.set(STEP, newSelectedStepId);
       queryParams.delete(TASK_RUN_DETAILS);
     } else {
       queryParams.delete(STEP);
       queryParams.set(TASK_RUN_DETAILS, true);
     }
 
-    const currentStepId = this.props.selectedStepId;
-    if (selectedStepId !== currentStepId) {
+    if (newSelectedStepId !== selectedStepId) {
       queryParams.delete(VIEW);
     }
 
     const browserURL = match.url.concat(`?${queryParams.toString()}`);
     history.push(browserURL);
-  };
+  }
 
-  showRerunNotification = value => {
-    this.setState({ showRerunNotification: value });
-  };
+  if (loading) {
+    return <SkeletonText heading width="60%" />;
+  }
 
-  fetchTaskAndRuns(taskRunName, namespace) {
-    this.props.fetchTaskRun({ name: taskRunName, namespace }).then(taskRun => {
-      if (taskRun && taskRun.spec.taskRef) {
-        const { name, kind } = taskRun.spec.taskRef;
-        this.props
-          .fetchTaskByType(name, taskTypeKeys[kind], namespace)
-          .then(() => {
-            this.setState({ loading: false });
-          });
-      } else {
-        this.setState({ loading: false });
-      }
+  if (error) {
+    return notification({
+      intl,
+      kind: 'error',
+      message: intl.formatMessage({
+        id: 'dashboard.taskRun.errorLoading',
+        defaultMessage: 'Error loading TaskRun'
+      })
     });
   }
 
-  render() {
-    const { loading, showRerunNotification } = this.state;
-    const {
-      error,
+  if (!taskRun) {
+    return notification({
       intl,
-      selectedStepId,
-      showTaskRunDetails,
-      task,
-      taskRun,
-      view
-    } = this.props;
-
-    if (loading) {
-      return <SkeletonText heading width="60%" />;
-    }
-
-    if (error) {
-      return TaskRunContainer.notification({
-        intl,
-        kind: 'error',
-        message: intl.formatMessage({
-          id: 'dashboard.taskRun.errorLoading',
-          defaultMessage: 'Error loading TaskRun'
-        })
-      });
-    }
-
-    if (!taskRun) {
-      return TaskRunContainer.notification({
-        intl,
-        kind: 'info',
-        message: intl.formatMessage({
-          id: 'dashboard.taskRun.unavailable',
-          defaultMessage: 'TaskRun not available'
-        })
-      });
-    }
-
-    const definition = getStepDefinition({
-      selectedStepId,
-      task,
-      taskRun
+      kind: 'info',
+      message: intl.formatMessage({
+        id: 'dashboard.taskRun.unavailable',
+        defaultMessage: 'TaskRun not available'
+      })
     });
+  }
 
-    const stepStatus = getStepStatus({
-      selectedStepId,
-      taskRun
-    });
+  const definition = getStepDefinition({
+    selectedStepId,
+    task,
+    taskRun
+  });
 
-    const {
-      reason: taskRunStatusReason,
-      message: taskRunStatusMessage,
-      status: succeeded
-    } = getStatus(taskRun);
+  const stepStatus = getStepStatus({
+    selectedStepId,
+    taskRun
+  });
 
-    const logContainer = this.getLogContainer({
-      stepName: selectedStepId,
-      stepStatus,
-      taskRun
-    });
+  const {
+    reason: taskRunStatusReason,
+    message: taskRunStatusMessage,
+    status: succeeded
+  } = getStatus(taskRun);
 
-    const onViewChange = getViewChangeHandler(this.props);
+  const logContainer = getLogContainer({
+    stepName: selectedStepId,
+    stepStatus,
+    taskRun
+  });
 
-    const rerun = !this.props.isReadOnly &&
-      !taskRun.metadata?.labels?.['tekton.dev/pipeline'] && (
-        <Rerun
-          getURL={({ name, namespace }) =>
-            urls.taskRuns.byName({ namespace, taskRunName: name })
+  const onViewChange = getViewChangeHandler(props);
+
+  const rerun = !isReadOnly &&
+    !taskRun.metadata?.labels?.['tekton.dev/pipeline'] && (
+      <Rerun
+        getURL={({ name, namespace: currentNamespace }) =>
+          urls.taskRuns.byName({
+            namespace: currentNamespace,
+            taskRunName: name
+          })
+        }
+        run={taskRun}
+        rerun={rerunTaskRun}
+        showNotification={setRerunNotification}
+      />
+    );
+
+  return (
+    <>
+      <div id="tkn--maximized-logs-container" ref={maximizedLogsContainer} />
+      {rerunNotification && (
+        <InlineNotification
+          lowContrast
+          actions={
+            rerunNotification.logsURL ? (
+              <Link
+                className="bx--inline-notification__text-wrapper"
+                to={rerunNotification.logsURL}
+              >
+                {intl.formatMessage({
+                  id: 'dashboard.run.rerunStatusMessage',
+                  defaultMessage: 'View status'
+                })}
+              </Link>
+            ) : (
+              ''
+            )
           }
-          run={taskRun}
-          rerun={rerunTaskRun}
-          showNotification={this.showRerunNotification}
+          title={rerunNotification.message}
+          kind={rerunNotification.kind}
+          caption=""
         />
-      );
-
-    return (
-      <>
-        <div
-          id="tkn--maximized-logs-container"
-          ref={this.maximizedLogsContainer}
+      )}
+      <RunHeader
+        lastTransitionTime={taskRun.status?.startTime}
+        loading={loading}
+        message={taskRunStatusMessage}
+        reason={taskRunStatusReason}
+        runName={taskRun.metadata.name}
+        status={succeeded}
+      >
+        {rerun}
+      </RunHeader>
+      <div className="tkn--tasks">
+        <TaskTree
+          onSelect={handleTaskSelected}
+          selectedStepId={selectedStepId}
+          selectedTaskId={showTaskRunDetails && taskRun.metadata.uid}
+          taskRuns={[taskRun]}
         />
-        {showRerunNotification && (
-          <InlineNotification
-            lowContrast
-            actions={
-              showRerunNotification.logsURL ? (
-                <Link
-                  className="bx--inline-notification__text-wrapper"
-                  to={showRerunNotification.logsURL}
-                >
-                  {intl.formatMessage({
-                    id: 'dashboard.run.rerunStatusMessage',
-                    defaultMessage: 'View status'
-                  })}
-                </Link>
-              ) : (
-                ''
-              )
-            }
-            title={showRerunNotification.message}
-            kind={showRerunNotification.kind}
-            caption=""
+        {(selectedStepId && (
+          <StepDetails
+            definition={definition}
+            logContainer={logContainer}
+            onViewChange={onViewChange}
+            showIO
+            stepName={selectedStepId}
+            stepStatus={stepStatus}
+            taskRun={taskRun}
+            view={view}
+          />
+        )) || (
+          <TaskRunDetails
+            onViewChange={onViewChange}
+            task={task}
+            taskRun={taskRun}
+            view={view}
           />
         )}
-        <RunHeader
-          lastTransitionTime={taskRun.status?.startTime}
-          loading={loading}
-          message={taskRunStatusMessage}
-          reason={taskRunStatusReason}
-          runName={taskRun.metadata.name}
-          status={succeeded}
-        >
-          {rerun}
-        </RunHeader>
-        <div className="tkn--tasks">
-          <TaskTree
-            onSelect={this.handleTaskSelected}
-            selectedStepId={selectedStepId}
-            selectedTaskId={showTaskRunDetails && taskRun.metadata.uid}
-            taskRuns={[taskRun]}
-          />
-          {(selectedStepId && (
-            <StepDetails
-              definition={definition}
-              logContainer={logContainer}
-              onViewChange={onViewChange}
-              showIO
-              stepName={selectedStepId}
-              stepStatus={stepStatus}
-              taskRun={taskRun}
-              view={view}
-            />
-          )) || (
-            <TaskRunDetails
-              onViewChange={onViewChange}
-              task={task}
-              taskRun={taskRun}
-              view={view}
-            />
-          )}
-        </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
 }
 
 TaskRunContainer.propTypes = {
@@ -395,7 +365,7 @@ function mapStateToProps(state, ownProps) {
   return {
     error: getTaskRunsErrorMessage(state),
     externalLogsURL: getExternalLogsURL(state),
-    isReadOnly: isReadOnly(state),
+    isReadOnly: selectIsReadOnly(state),
     namespace,
     selectedStepId,
     isLogStreamingEnabled: selectIsLogStreamingEnabled(state),
@@ -408,9 +378,9 @@ function mapStateToProps(state, ownProps) {
 }
 
 const mapDispatchToProps = {
-  fetchTaskByType,
-  fetchTask,
-  fetchTaskRun
+  fetchTaskByType: fetchTaskByTypeActionCreator,
+  fetchTask: fetchTaskActionCreator,
+  fetchTaskRun: fetchTaskRunActionCreator
 };
 
 export default connect(
