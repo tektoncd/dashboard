97d983f043ea7a1f23d88d408c992f9f981bc789
Alan Greene <github.com@alangreene.net>
2021-06-09 17:52:34 +0100

    Add initial react-query utilities
    Add a custom hook for fetching a list of namespaced resources
    similar to our existing `fetchNamespacedCollection` action creator.
    The difference with this is that we won't need action creators,
    selectors, or the reducer backing it, all of the caching functionality
    is handled internally by react-query while giving us control over
    config (e.g. cache time, stale time, retries) if desired.
    
    Add a custom hook to handle websocket events, updating the react-query
    cache directly for any matching queries. This is a replacement for
    the existing websocket middleware and events handlers we've added with
    redux.
    
    These are not yet wired into the application, this will be handled
    in another PR.

--- src/api/utils.js
+++ src/api/utils.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2019-2020 The Tekton Authors
+Copyright 2019-2021 The Tekton Authors
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -11,6 +11,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+import React, { useContext, useEffect } from 'react';
+import { useQuery, useQueryClient } from 'react-query';
 import { ALL_NAMESPACES } from '@tektoncd/dashboard-utils';
 
 import { getAPIRoot } from './comms';
@@ -99,4 +101,57 @@ export function getTektonAPI(
     { group, version, type, name, namespace },
     queryParams
   );
+}
+
+export const WebSocketContext = React.createContext();
+
+function getResourceVersion(resource) {
+  return parseInt(resource.metadata.resourceVersion, 10);
+}
+
+export function useWebSocket(resourceType) {
+  const webSocket = useContext(WebSocketContext);
+  const queryClient = useQueryClient();
+  useEffect(() => {
+    const eventListener = event => {
+      if (event.type !== 'message') {
+        return;
+      }
+      const { operation, payload } = JSON.parse(event.data);
+      queryClient.setQueriesData(resourceType, data => {
+        switch (operation) {
+          case 'Created':
+            const existingResource = data.find(
+              resource => resource.metadata.uid === payload.metadata.uid
+            );
+            if (existingResource) {
+              return data; // payload is stale, ignore it
+            }
+            return [...data, payload];
+          case 'Deleted':
+            return data.filter(
+              resource => resource.metadata.uid !== payload.metadata.uid
+            );
+          case 'Updated':
+            return data.map(resource => {
+              return resource.metadata.uid === payload.metadata.uid &&
+                // only apply the update if it's newer than the version we already have
+                getResourceVersion(payload) > getResourceVersion(resource)
+                ? payload
+                : resource;
+            });
+          default:
+            return data;
+        }
+      });
+    };
+    webSocket.addEventListener('message', eventListener);
+    return () => webSocket.removeEventListener('message', eventListener);
+  }, [queryClient, webSocket]);
+}
+
+export function useNamespacedCollection(resourceType, api, params) {
+  const query = useQuery([resourceType, params], () => api(params));
+  useWebSocket(resourceType);
+  return query;
 }
