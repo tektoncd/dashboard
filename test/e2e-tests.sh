d8d5de77c94cbaf4931f6be2766637b944b72cfc
Alan Greene <github.com@alangreene.net>
2021-12-13 11:11:19 +0000

    Update tests to latest Pipelines and Triggers releases
    Update readme compatibility table to reflect latest versions

--- test/e2e-tests.sh
+++ test/e2e-tests.sh
@@ -219,11 +219,11 @@ if [ -z "$SKIP_BUILD_TEST" ]; then
 fi
 
 if [ -z "$PIPELINES_VERSION" ]; then
-  export PIPELINES_VERSION=v0.29.0
+  export PIPELINES_VERSION=v0.30.0
 fi
 
 if [ -z "$TRIGGERS_VERSION" ]; then
-  export TRIGGERS_VERSION=v0.17.0
+  export TRIGGERS_VERSION=v0.17.1
 fi
 
 header "Installing Pipelines and Triggers"
