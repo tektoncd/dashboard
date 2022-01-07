28dbf4be81301bcb67043fe40e05f6f72b51449e
Alan Greene <github.com@alangreene.net>
2022-01-07 16:22:13 +0000

    Improve and simplify webpack build
    Ensure we use consistent hashing method for vendor bundle to avoid
    unnecessary rebuilds.
    
    Remove the `build_ko` npm script as we no longer need to support two
    different locations for the build output. This was leftover from
    some very early releases of the Dashboard. The webpack build output
    should always be placed in the `kodata` folder. Update docs to reflect
    the change.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2019-2021 The Tekton Authors
+Copyright 2019-2022 The Tekton Authors
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -24,10 +24,11 @@ module.exports = merge(common({ mode }), {
   mode,
   output: {
     filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'cmd/dashboard/kodata'),
     publicPath: './'
   },
   optimization: {
+    moduleIds: 'deterministic',
     runtimeChunk: 'single',
     splitChunks: {
       cacheGroups: {
