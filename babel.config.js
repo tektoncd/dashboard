172718fe7fb0d22f20e840f622e539cd2503080e
Alan Greene <github.com@alangreene.net>
2022-01-26 12:48:29 +0000

    Fix for Safari
    A combination of updates in some transitive dependencies broke
    the UI in Safari due to use of unsupported regex lookbehind. This
    feature has been supported in the other major browsers for quite some
    time but Safari has lagged behind.
    
    Our switch to Node 16 and npm 8 introduced some unexpected updates
    in transitive dependencies, namely:
    `git-url-parse > git-up > parse-url > normalize-url`
    
    Due to the version ranges specified by these packages we ended up
    pulling in `normalize-url@6.x` which is not compatible with Safari.
    We cannot force an update to `normalize-url@7.x` which contains the fix
    due to incompatibility with `parse-url` (`normalize-url` has been converted
    to an ESM-only package).
    
    As a workaround, pin `git-up` to the known working version from before
    our Node 16 update. Hopefully we can remove this in the near future once
    issues with `parse-url` and `normalize-url` are resolved.

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2019 The Tekton Authors
+Copyright 2019-2022 The Tekton Authors
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -19,7 +19,7 @@ module.exports = api => {
       [
         '@babel/preset-env',
         {
-          corejs: '3.6',
+          corejs: '3.20',
           exclude: ['@babel/plugin-transform-regenerator'],
           modules: false,
           useBuiltIns: 'entry'
