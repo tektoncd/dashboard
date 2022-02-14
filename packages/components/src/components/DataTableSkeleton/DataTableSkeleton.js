3038edb0613084635ce8ae402052637f5c990a08
Alan Greene <github.com@alangreene.net>
2022-02-14 12:43:51 +0000

    Avoid use of concatenation when producing CSS classnames
    Dynamically concatenating values to produce CSS classnames makes it
    difficult to identify which values are / may be used, often leading
    to stale styles being left behind as developers are unsure whether
    they're safe to remove.
    
    Where possible we should ensure the entire CSS classname appears
    as a single string in the codebase so it's easily searchable. This
    also becomes increasingly important when using tooling to identify
    unused styles.
    
    Some components such as `LogFormat` are exceptions to this rule
    due to the potentially large number of values involved, and the
    affected classnames already have an easily searchable prefix. These
    prefixes can be excluded from the tooling to avoid them being pruned
    as they depend on user content only available at runtime.

--- packages/components/src/components/DataTableSkeleton/DataTableSkeleton.js
+++ packages/components/src/components/DataTableSkeleton/DataTableSkeleton.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2020-2021 The Tekton Authors
+Copyright 2020-2022 The Tekton Authors
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -14,6 +14,14 @@ limitations under the License.
 
 import PropTypes from 'prop-types';
 import React from 'react';
+
+const tableSizeClassNames = {
+  xs: 'bx--data-table--xs',
+  sm: 'bx--data-table--sm',
+  md: 'bx--data-table--md',
+  lg: 'bx--data-table--lg',
+  xl: 'bx--data-table--xl'
+};
 
 const DataTableSkeleton = ({
   className,
@@ -35,7 +43,7 @@ const DataTableSkeleton = ({
     </tr>
   ));
 
-  const sizeClassName = size ? `bx--data-table--${size}` : '';
+  const sizeClassName = tableSizeClassNames[size] || '';
 
   return (
     <table
