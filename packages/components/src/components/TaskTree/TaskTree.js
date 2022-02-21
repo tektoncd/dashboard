8e5bd70abc0a11bab468526f9250a41813742d27
Alan Greene <github.com@alangreene.net>
2022-02-21 14:34:25 +0000

    Fix error in loading state when TaskRun hasn't fully started yet

--- packages/components/src/components/TaskTree/TaskTree.js
+++ packages/components/src/components/TaskTree/TaskTree.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2019-2021 The Tekton Authors
+Copyright 2019-2022 The Tekton Authors
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -36,7 +36,7 @@ const TaskTree = ({ onSelect, selectedStepId, selectedTaskId, taskRuns }) => {
           [labelConstants.DASHBOARD_RETRY_NAME]: retryName
         } = labels;
         const { reason, status } = getStatus(taskRun);
-        const { steps } = taskRun.status;
+        const { steps } = taskRun.status || {};
         const expanded =
           (!selectedTaskId && erroredTask?.metadata.uid === uid) ||
           selectedTaskId === uid ||
