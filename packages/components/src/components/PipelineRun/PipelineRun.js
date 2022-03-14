06b16659f2ddd385b0ce6d87420f54044f0872d9
Alan Greene <github.com@alangreene.net>
2022-03-14 16:20:10 +0000

    Add support for rendering PipelineRun header before a complete resource is available
    Some consumers of the Dashboard components provide their own queueing / pipeline orchestration
    layer in front of Tekton Pipelines. In this case the PipelineRun resource they attempt to
    render may not be complete (i.e. may not contain complete metadata or status).
    
    Make a best effort attempt to render the PipelineRun details based on the data provided.
    For example, if metadata is incomplete but there is a `generateName` field, use this as
    a temporary display name until the 'real' run resource is available.
    
    This allows for display of additional information that may be provided by the consumer
    such as the triggering event or other payload information.

--- packages/components/src/components/PipelineRun/PipelineRun.js
+++ packages/components/src/components/PipelineRun/PipelineRun.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2019-2021 The Tekton Authors
+Copyright 2019-2022 The Tekton Authors
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -242,7 +242,8 @@ export /* istanbul ignore next */ class PipelineRunContainer extends Component {
       );
     }
 
-    const pipelineRunName = pipelineRun.metadata.name;
+    const pipelineRunName =
+      pipelineRun.metadata.name || pipelineRun.metadata.generateName;
     const pipelineRunError = this.getPipelineRunError();
 
     const {
