a17c4c898879a978b7379349bc4051c6dd9763d0
Alan Greene <github.com@alangreene.net>
2022-09-05 18:57:56 +0100

    Add e2e tests to verify we have permission to read Tekton resources
    - Add missing copyright header to Dockerfile
    - Update About test to add missing assertions
    - Add new test to ensure the UI loads correctly for the standard
      set of Tekton (Pipelines + Triggers) resources, and handles
      the error case for missing permissions as expected

---
+++ packages/e2e/cypress/e2e/common/read-permissions.cy.js
@@ -0,0 +1,42 @@
+/*
+Copyright 2022 The Tekton Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import kinds from '../../fixtures/kinds.json';
+
+describe('Read permissions', () => {
+  before(() => {
+    cy.visit('/');
+    cy.hash().should('equal', '#/about');
+    cy.get('h1').contains('About Tekton');
+  });
+
+  kinds.forEach(kind => {
+    it(`should display ${kind.label} page`, () => {
+      cy.get('.bx--side-nav a')
+        .contains(new RegExp(`^${kind.label}$`))
+        .click();
+      cy.hash().should('equal', `#${kind.path}`);
+      cy.get('h1').contains(kind.label);
+      cy.get('table');
+      cy.get('table.tkn--data-table-skeleton').should('not.exist');
+      cy.contains('Forbidden').should('not.exist');
+    });
+  });
+
+  it('should display error for resources missing permissions', () => {
+    const groupVersionKind = 'tekton.dev/v1alpha1/fake-kind';
+    cy.visit(`/#/${groupVersionKind}`);
+    cy.get('h1').contains(groupVersionKind);
+    cy.contains('Forbidden');
+  });
+});
