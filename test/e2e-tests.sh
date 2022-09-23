df314c1057ce1f7e222b709161da44e0b64947f7
Alan Greene <github.com@alangreene.net>
2022-09-23 14:12:43 +0100

    Update tests to use latest Pipelines release

--- test/e2e-tests.sh
+++ test/e2e-tests.sh
@@ -198,7 +198,7 @@ header "Building browser E2E image"
 docker build -t dashboard-e2e packages/e2e
 
 if [ -z "$PIPELINES_VERSION" ]; then
-  export PIPELINES_VERSION=v0.39.0
+  export PIPELINES_VERSION=v0.40.0
 fi
 
 if [ -z "$TRIGGERS_VERSION" ]; then
