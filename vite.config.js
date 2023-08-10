8698235f7f40a32dd2d2e62c680295fb169e98b7
Alan Greene <github.com@alangreene.net>
2023-08-10 19:37:22 +0100

    Switch from webpack to vite
    This is the first of a number of commits to switch the Dashboard
    from using webpack + babel to using vite. This will simplify
    maintenance as there are significantly fewer dependencies involved,
    and should also provide some performance and other benefits of a
    more modern toolset with updated defaults for modern browsers.
    
    This initial commit gets the application building and running in
    both dev and production modes, but results in tests and a number of
    other scripts being broken. They will be addressed in subsequent
    commits, to be merged in the same PR.
    
    - Install vite and related plugins
    - Add base vite config including replacement for webpack-dev-server,
      and production minification (using defaults for now)
    - Rename all files containing JSX to have a .jsx extension
      This is required by vite by default, and helps with performance
      as it only runs the React / JSX processing on those files
    - Update format of YAML files to conform to expectations of the YAML plugin
    - Move the HTML template and index file to the root as expected by vite
    - Replace use of `export default from …` with `export { default } from …`
      which is more standard and doesn't require additional config / plugins
    - Replace use of `process.env` with `import.meta.env`
    - Replace webpack magic comments for dynamic imports
    - Update npm scripts to use vite instead of webpack for building
    - Add new script for enabling pseudo-locale for i18n testing
    - Add new preview script to serve the production bundles from the
      dev server, allowing for quicker iteration (reduces need to build
      and deploy the Dashboard backend during development)

---
+++ vite.config.js
@@ -0,0 +1,66 @@
+/*
+Copyright 2023 The Tekton Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import { defineConfig } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
+import react from '@vitejs/plugin-react-swc';
+import svgr from 'vite-plugin-svgr';
+import yaml from '@modyfi/vite-plugin-yaml';
+
+const customAPIDomain = process.env.API_DOMAIN;
+
+const proxyConfig = {
+  changeOrigin: !!customAPIDomain,
+  target: customAPIDomain || 'http://localhost:9097'
+};
+
+export default defineConfig(({ _mode }) => ({
+  root: './',
+  base: './',
+  build: {
+    // Relative to outDir
+    assetsDir: '.',
+    // Relative to the root
+    outDir: 'cmd/dashboard/kodata'
+  },
+  css: {
+    devSourcemap: true
+  },
+  esbuild: {
+    target: 'es2021'
+  },
+  plugins: [
+    createHtmlPlugin({}),
+    react(),
+    svgr(),
+    yaml()
+  ],
+  server: {
+    headers: {
+      // https://github.com/codemirror/codemirror5/issues/6707
+      // style-src blob: 'nonce-tkn-dev';
+      'Content-Security-Policy':
+        "default-src 'none'; style-src 'self' 'unsafe-inline'; img-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; connect-src 'self' wss: ws:; font-src 'self' https://1.www.s81c.com;"
+    },
+    open: true,
+    port: process.env.PORT || 8000,
+    proxy: {
+      '/v1': proxyConfig,
+      '/api': {
+        ...proxyConfig,
+        ws: true
+      }
+    },
+    strictPort: true
+  }
+}));
