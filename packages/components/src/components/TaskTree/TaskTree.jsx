13f53e1750b8487b1e4ce14ead3446123d2d94d5
Alan Greene <github.com@alangreene.net>
2024-01-10 13:18:06 +0000

    Add support for optional pipeline prop in PipelineRun component
    Some consumers of the PipelineRun component modify the PipelineRun
    resource (e.g. by removing `status.pipelineSpec`) to reduce message
    size and content duplication for very large pipeline definitions.
    
    As a result, existing support for displaying matrix tasks on the
    PipelineRun details page was broken, preventing the user from
    selecting the second or subsequent TaskRuns for a given matrix task.
    
    Add support for providing an optional `pipeline` prop which can
    be used as fallback in case neither `PipelineRun.spec.pipelineSpec`
    nor `PipelineRun.status.pipelineSpec` are available.

--- packages/components/src/components/TaskTree/TaskTree.jsx
+++ packages/components/src/components/TaskTree/TaskTree.jsx
@@ -1,5 +1,5 @@
 /*
-Copyright 2019-2023 The Tekton Authors
+Copyright 2019-2024 The Tekton Authors
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -99,7 +99,7 @@ const TaskTree = ({
             selectedStepId={selectedStepId}
             steps={steps}
             succeeded={status}
-            taskRun={taskRun}
+            taskRun={taskRunToUse}
           />
         );
       })}
