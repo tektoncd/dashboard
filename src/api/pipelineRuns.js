98315da1ae924defac8ef821db2f50c5704e65c2
Alan Greene <github.com@alangreene.net>
2024-02-02 14:01:25 +0000

    Fix for Create PipelineRun specifying ServiceAccount with v1 API

--- src/api/pipelineRuns.js
+++ src/api/pipelineRuns.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2019-2023 The Tekton Authors
+Copyright 2019-2024 The Tekton Authors
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -95,8 +95,10 @@ export function getPipelineRunPayload({
   timeoutsPipeline,
   timeoutsTasks
 }) {
+  const pipelinesAPIVersion = getTektonPipelinesAPIVersion();
+
   const payload = {
-    apiVersion: `tekton.dev/${getTektonPipelinesAPIVersion()}`,
+    apiVersion: `tekton.dev/${pipelinesAPIVersion}`,
     kind: 'PipelineRun',
     metadata: {
       name: pipelineRunName,
@@ -128,7 +130,13 @@ export function getPipelineRunPayload({
     };
   }
   if (serviceAccount) {
-    payload.spec.serviceAccountName = serviceAccount;
+    if (pipelinesAPIVersion === 'v1') {
+      payload.spec.taskRunTemplate = {
+        serviceAccountName: serviceAccount
+      };
+    } else {
+      payload.spec.serviceAccountName = serviceAccount;
+    }
   }
   if (timeoutsFinally || timeoutsPipeline || timeoutsTasks) {
     payload.spec.timeouts = {
