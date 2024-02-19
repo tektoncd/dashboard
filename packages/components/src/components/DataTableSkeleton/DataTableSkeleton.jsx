97700646be7728e36f01120131da8620ee69122f
Alan Greene <github.com@alangreene.net>
2024-02-19 13:42:09 +0000

    Update eslint-plugin-jsx-a11y to latest version
    Include workaround for bug where it's incorrectly flagging non-form
    control elements for `jsx-a11y/control-has-associated-label`

--- packages/components/src/components/DataTableSkeleton/DataTableSkeleton.jsx
+++ packages/components/src/components/DataTableSkeleton/DataTableSkeleton.jsx
@@ -1,5 +1,5 @@
 /*
-Copyright 2020-2023 The Tekton Authors
+Copyright 2020-2024 The Tekton Authors
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -11,6 +11,10 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 /* istanbul ignore file */
+
+// disable `control-has-associated-label` check due to a bug in the jsx-a11y
+// plugin incorrectly flagging this on elements that are not form controls
+/* eslint-disable jsx-a11y/control-has-associated-label */
 
 import PropTypes from 'prop-types';
 import React from 'react';
