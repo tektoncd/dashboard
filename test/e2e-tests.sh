eb4b1a4f311d330f6ea840099867bb2c0ab6890f
Alan Greene <github.com@alangreene.net>
2024-06-26 10:55:39 +0100

    Update E2E tests to Pipelines v0.61.0

--- test/e2e-tests.sh
+++ test/e2e-tests.sh
@@ -116,7 +116,7 @@ DOCKER_BUILDKIT=1 docker build -t dashboard-e2e packages/e2e || fail_test "Faile
 echo "Finished at $(date)"
 
 if [ -z "$PIPELINES_VERSION" ]; then
-  export PIPELINES_VERSION=v0.60.1
+  export PIPELINES_VERSION=v0.61.0
 fi
 
 if [ -z "$TRIGGERS_VERSION" ]; then
