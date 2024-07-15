25af3cfca325fb9a854d9edc7a91c9e56ea8f061
Alan Greene <github.com@alangreene.net>
2024-07-15 15:45:13 +0100

    Fix TooltipDropdown stories

--- packages/components/src/components/TooltipDropdown/TooltipDropdown.stories.jsx
+++ packages/components/src/components/TooltipDropdown/TooltipDropdown.stories.jsx
@@ -19,6 +19,7 @@ export default {
     label: 'Select an item',
     items: ['item 1', 'item 2', 'item 3', 'item 4', 'item 5', 'item 6'],
     loading: false,
+    onChange: () => {},
     titleText: ''
   },
   component: TooltipDropdown,
