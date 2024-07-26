79ad9f2a63afa527641bb4d85d250ed0371ba89e
Alan Greene <github.com@alangreene.net>
2024-07-26 17:50:58 +0100

    Update E2E tests to latest Pipelines and Triggers releases

--- test/e2e-tests.sh
+++ test/e2e-tests.sh
@@ -116,11 +116,11 @@ DOCKER_BUILDKIT=1 docker build -t dashboard-e2e packages/e2e || fail_test "Faile
 echo "Finished at $(date)"
 
 if [ -z "$PIPELINES_VERSION" ]; then
-  export PIPELINES_VERSION=v0.61.0
+  export PIPELINES_VERSION=v0.62.0
 fi
 
 if [ -z "$TRIGGERS_VERSION" ]; then
-  export TRIGGERS_VERSION=v0.27.0
+  export TRIGGERS_VERSION=v0.28.0
 fi
 
 header "Installing Pipelines and Triggers"
