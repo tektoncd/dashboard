401af736b760bcb9c811eba4072136fa2748c6ad
Alan Greene <github.com@alangreene.net>
2025-01-03 21:34:17 +0000

    Update E2E tests to Triggers v0.30.1

--- test/e2e-tests.sh
+++ test/e2e-tests.sh
@@ -120,7 +120,7 @@ if [ -z "$PIPELINES_VERSION" ]; then
 fi
 
 if [ -z "$TRIGGERS_VERSION" ]; then
-  export TRIGGERS_VERSION=v0.30.0
+  export TRIGGERS_VERSION=v0.30.1
 fi
 
 header "Installing Pipelines and Triggers"
