d05f8a7bb3b7ebe16b218036c0a1b88aa3890b7a
Alan Greene <github.com@alangreene.net>
2025-02-14 15:05:03 +0000

    Update log timestamp detection to support more formats
    Update the regex to support detection of:
    - timestamps with no fractional seconds
    - timestamps with no seconds
    - timezone offsets (+/-hh:mm)

--- packages/components/src/components/Log/Log.jsx
+++ packages/components/src/components/Log/Log.jsx
@@ -31,8 +31,8 @@ import LogFormat from '../LogFormat';
 const itemSize = 16; // This should be kept in sync with the line-height in SCSS
 const defaultHeight = itemSize * 100 + itemSize / 2;
 
-const logFormatRegex =
-  /^((?<timestamp>\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3,9}Z)\s?)?(::(?<command>group|endgroup|error|warning|info|notice|debug)::)?(?<message>.*)?$/s;
+export const logFormatRegex =
+  /^((?<timestamp>\d{4}-\d{2}-\d{2}T\d{2}:\d{2}(:\d{2})?(.\d{3,9})?(Z|[+-]\d{2}:\d{2}))\s?)?(::(?<command>group|endgroup|error|warning|info|notice|debug)::)?(?<message>.*)?$/s;
 
 function LogsFilteredNotification({ displayedLogLines, totalLogLines }) {
   const intl = useIntl();
