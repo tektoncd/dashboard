fec11c6d4c1b9c5e1221c66d4779c18233af894b
Brian Gleeson <brian.gleeson@ie.ibm.com>
2025-03-07 13:00:55 +0000

    Fix dashboard-utils import issue
    When importing dashboard-utils into a commonjs file, a compiler
    error occurs due to an ESM vs CJS conflict. Using a dynamic
    import as a workaround for this results in an error, as files
    imported in packages/utils/src/index.js cant be found. Adding
    the extension to these imports resolves this problem

--- packages/utils/src/utils/router.js
+++ packages/utils/src/utils/router.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2019-2024 The Tekton Authors
+Copyright 2019-2025 The Tekton Authors
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -10,8 +10,11 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+/* eslint-disable import/extensions */
 import { generatePath } from 'react-router-dom';
-import { labels } from './constants';
+// Include .js extension in import to solve ES-modules and CommonJS
+// problem when importing dashboard-utils
+import { labels } from './constants.js';
 
 function byNamespace({ path = '' } = {}) {
   return `/namespaces/:namespace${path}`;
