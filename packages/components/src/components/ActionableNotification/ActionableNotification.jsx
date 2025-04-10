ab7fef2ae779eeacc111fe9c694b78d7413a4adb
Alan Greene <github.com@alangreene.net>
2025-04-10 13:11:36 +0100

    Switch to stable export for Carbon FeatureFlags
    `@carbon/react@1.80.0` now provides a stable export for the `FeatureFlags`
    component. Switch to this now so we're prepared for the removal of the
    unstable export in a future release.

--- packages/components/src/components/ActionableNotification/ActionableNotification.jsx
+++ packages/components/src/components/ActionableNotification/ActionableNotification.jsx
@@ -1,5 +1,5 @@
 /*
-Copyright 2024 The Tekton Authors
+Copyright 2024-2025 The Tekton Authors
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -13,7 +13,7 @@ limitations under the License.
 
 import {
   ActionableNotification as CarbonActionableNotification,
-  unstable_FeatureFlags as FeatureFlags
+  FeatureFlags
 } from '@carbon/react';
 
 export default function ActionableNotification(props) {
