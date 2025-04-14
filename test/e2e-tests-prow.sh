a557cc464175cd0b17a5168bde15d3c220b6a81d
Alan Greene <github.com@alangreene.net>
2025-04-14 19:15:17 +0100

    Remove dependency on setup-kind script from pipeline repo
    We don't need most of the functionality provided by the setup-kind script
    in the Pipelines repo (e.g. local registry, metallb, etc.), and it creates
    overhead in managing the supported k8s versions for the tests in the
    Dashboard repo.
    
    Instead, adopt a simplified approach to creating the desired cluster and
    directly manage the supported k8s versions in this repo.
    
    As part of this change we also update the k8s versions used to the latest
    supported releases.

--- test/e2e-tests-prow.sh
+++ test/e2e-tests-prow.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright 2022 The Tekton Authors
+# Copyright 2022-2025 The Tekton Authors
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -13,4 +13,70 @@
 
 set -e
 
+# Defaults
+K8S_VERSION="v1.32.x"
+
+while [[ $# -ne 0 ]]; do
+  parameter="$1"
+  case "${parameter}" in
+    --k8s-version)
+      shift
+      K8S_VERSION="$1"
+      ;;
+    *) abort "unknown option ${parameter}" ;;
+  esac
+  shift
+done
+
+# The version map correlated with this version of kind
+case ${K8S_VERSION} in
+  v1.29.x)
+    K8S_VERSION="1.29.14"
+    KIND_IMAGE_SHA="sha256:8703bd94ee24e51b778d5556ae310c6c0fa67d761fae6379c8e0bb480e6fea29"
+    KIND_IMAGE="kindest/node:${K8S_VERSION}@${KIND_IMAGE_SHA}"
+    ;;
+  v1.30.x)
+    K8S_VERSION="1.30.10"
+    KIND_IMAGE_SHA="sha256:4de75d0e82481ea846c0ed1de86328d821c1e6a6a91ac37bf804e5313670e507"
+    KIND_IMAGE="kindest/node:${K8S_VERSION}@${KIND_IMAGE_SHA}"
+    ;;
+  v1.31.x)
+    K8S_VERSION="1.31.6"
+    KIND_IMAGE_SHA="sha256:28b7cbb993dfe093c76641a0c95807637213c9109b761f1d422c2400e22b8e87"
+    KIND_IMAGE="kindest/node:${K8S_VERSION}@${KIND_IMAGE_SHA}"
+    ;;
+  v1.32.x)
+    K8S_VERSION="1.32.2"
+    KIND_IMAGE_SHA="sha256:f226345927d7e348497136874b6d207e0b32cc52154ad8323129352923a3142f"
+    KIND_IMAGE="kindest/node:${K8S_VERSION}@${KIND_IMAGE_SHA}"
+    ;;
+  *) abort "Unsupported version: ${K8S_VERSION}" ;;
+esac
+
+# Create kind config with correct k8s version
+cat > kind-config.yaml <<EOF
+apiVersion: kind.x-k8s.io/v1alpha4
+kind: Cluster
+nodes:
+- role: control-plane
+  image: "docker.io/${KIND_IMAGE}"
+EOF
+
+echo '::group::kind-config.yaml'
+cat kind-config.yaml
+echo '::endgroup::'
+
+echo '::group::kind version'
+kind --version
+echo '::endgroup::'
+
+echo '::group::check we can talk to docker'
+docker ps
+echo '::endgroup::'
+
+echo '::group::create cluster'
+kind create cluster --config kind-config.yaml
+echo '::endgroup::'
+
+# Run the tests
 $(git rev-parse --show-toplevel)/test/presubmit-tests.sh --integration-tests;
