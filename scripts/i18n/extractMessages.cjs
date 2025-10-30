8891f565c6b679f1c8ca4511e815f51de568f1c4
BRIAN GLEESON <Brian.Gleeson@ie.ibm.com>
2025-10-30 14:32:35 +0000

    Fix ban-dependencies issues

--- scripts/i18n/extractMessages.cjs
+++ scripts/i18n/extractMessages.cjs
@@ -10,10 +10,11 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-require('dotenv').config();
 
+// eslint-disable-next-line depend/ban-dependencies
 const difference = require('lodash.difference');
 const fs = require('fs');
+// eslint-disable-next-line depend/ban-dependencies
 const omit = require('lodash.omit');
 const path = require('path');
 
