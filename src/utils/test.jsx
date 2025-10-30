b66c5a9138729539cca399192dbce9641c3ae3fe
BRIAN GLEESON <Brian.Gleeson@ie.ibm.com>
2025-10-30 09:54:29 +0000

    Remove deprecated custom logger

--- src/utils/test.jsx
+++ src/utils/test.jsx
@@ -30,11 +30,6 @@ export function getQueryClient() {
         retry: false,
         staleTime: Infinity
       }
-    },
-    logger: {
-      log: console.log,
-      warn: console.warn,
-      error: () => {}
     }
   });
 }
