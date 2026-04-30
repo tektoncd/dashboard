4fd50d3625151e69520190dc17ca90a9280056a4
Alan Greene <github.com@alangreene.net>
2026-04-30 22:23:44 +0100

    Update kind and k8s versions for E2E tests and dev scripts
    Update to the latest `kind` release and update the k8s node images
    used for E2E and the dev `prepare-kind-cluster` script. The latest
    released node image for kind is currently 1.35.x, there is no release
    for 1.36 yet.
    
    For the E2E jobs, rename `k8s-plus-one` to `k8s-latest` to better
    reflect its purpose.

--- test/e2e-tests-prow.sh
+++ test/e2e-tests-prow.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright 2022-2025 The Tekton Authors
+# Copyright 2022-2026 The Tekton Authors
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -14,7 +14,7 @@
 set -e
 
 # Defaults
-K8S_VERSION="v1.32.x"
+K8S_VERSION="v1.35.x"
 
 while [[ $# -ne 0 ]]; do
   parameter="$1"
@@ -29,29 +29,33 @@ while [[ $# -ne 0 ]]; do
 done
 
 # The version map correlated with this version of kind
+# Image versions and SHAs can be found in the kind release notes
+# https://github.com/kubernetes-sigs/kind/releases
 case ${K8S_VERSION} in
-  v1.29.x)
-    K8S_VERSION="1.29.14"
-    KIND_IMAGE_SHA="sha256:8703bd94ee24e51b778d5556ae310c6c0fa67d761fae6379c8e0bb480e6fea29"
-    KIND_IMAGE="kindest/node:${K8S_VERSION}@${KIND_IMAGE_SHA}"
-    ;;
-  v1.30.x)
-    K8S_VERSION="1.30.10"
-    KIND_IMAGE_SHA="sha256:4de75d0e82481ea846c0ed1de86328d821c1e6a6a91ac37bf804e5313670e507"
-    KIND_IMAGE="kindest/node:${K8S_VERSION}@${KIND_IMAGE_SHA}"
-    ;;
   v1.31.x)
-    K8S_VERSION="1.31.6"
-    KIND_IMAGE_SHA="sha256:28b7cbb993dfe093c76641a0c95807637213c9109b761f1d422c2400e22b8e87"
-    KIND_IMAGE="kindest/node:${K8S_VERSION}@${KIND_IMAGE_SHA}"
+    K8S_VERSION="1.31.14"
+    KIND_IMAGE_SHA="sha256:6f86cf509dbb42767b6e79debc3f2c32e4ee01386f0489b3b2be24b0a55aac2b"
     ;;
   v1.32.x)
-    K8S_VERSION="1.32.2"
-    KIND_IMAGE_SHA="sha256:f226345927d7e348497136874b6d207e0b32cc52154ad8323129352923a3142f"
-    KIND_IMAGE="kindest/node:${K8S_VERSION}@${KIND_IMAGE_SHA}"
+    K8S_VERSION="1.32.11"
+    KIND_IMAGE_SHA="sha256:5fc52d52a7b9574015299724bd68f183702956aa4a2116ae75a63cb574b35af8"
+    ;;
+  v1.33.x)
+    K8S_VERSION="1.33.7"
+    KIND_IMAGE_SHA="sha256:d26ef333bdb2cbe9862a0f7c3803ecc7b4303d8cea8e814b481b09949d353040"
+    ;;
+  v1.34.x)
+    K8S_VERSION="1.34.3"
+    KIND_IMAGE_SHA="sha256:08497ee19eace7b4b5348db5c6a1591d7752b164530a36f855cb0f2bdcbadd48"
+    ;;
+  v1.35.x)
+    K8S_VERSION="1.35.0"
+    KIND_IMAGE_SHA="sha256:452d707d4862f52530247495d180205e029056831160e22870e37e3f6c1ac31f"
     ;;
   *) abort "Unsupported version: ${K8S_VERSION}" ;;
 esac
+
+KIND_IMAGE="kindest/node:${K8S_VERSION}@${KIND_IMAGE_SHA}"
 
 # Create kind config with correct k8s version
 cat > kind-config.yaml <<EOF
