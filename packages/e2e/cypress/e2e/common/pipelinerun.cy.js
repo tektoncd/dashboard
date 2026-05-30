9ff0f6f67a1e4d30f1291bc754d844d705e3b0ac
Alan Greene <github.com@alangreene.net>
2026-05-30 14:44:35 +0100

    Update E2E to Pipelines v1.13
    PipelineRuns and TaskRuns are consistently slower to execute with
    Pipelines v1.13. Increase the timeouts waiting for the successful
    status so the tests can pass.

--- packages/e2e/cypress/e2e/common/pipelinerun.cy.js
+++ packages/e2e/cypress/e2e/common/pipelinerun.cy.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2023-2025 The Tekton Authors
+Copyright 2023-2026 The Tekton Authors
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -54,7 +54,7 @@ spec:
     cy.get(`[title=${pipelineRunName}]`).click();
 
     cy.get('header[class="tkn--pipeline-run-header"]')
-      .find('span[class="tkn--status-label"]', { timeout: 15000 })
+      .find('span[class="tkn--status-label"]', { timeout: 60000 })
       .should('have.text', 'Succeeded');
 
     cy.contains('.tkn--log', 'Hello World!');
