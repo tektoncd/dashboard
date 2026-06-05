bed063e10237e8a1f79286904a0e29a4c4c17f70
Anamika Agrawal <anamikaaggarwal43@gmail.com>
2026-06-05 22:55:04 +0530

    Fix accessibility in date and duration components
    Use semantic HTML with aria-hidden="true" for visual content and cds--assistive-text
    for screen readers.
    
    Example - Duration display:
    - Visual: 2m 55s
    - Screen reader/Tooltip: Duration: 2 minutes 55 seconds
    
    Fixes #5018

--- packages/components/src/components/FormattedDuration/FormattedDuration.jsx
+++ packages/components/src/components/FormattedDuration/FormattedDuration.jsx
@@ -1,5 +1,5 @@
 /*
-Copyright 2019-2024 The Tekton Authors
+Copyright 2019-2026 The Tekton Authors
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -42,33 +42,14 @@ class FormattedDurationWrapper extends Component {
   state = { tooltip: '' };
 
   componentDidMount() {
-    const { intl } = this.props;
-    const tooltip = intl.formatMessage(
-      {
-        id: 'dashboard.run.duration',
-        defaultMessage: 'Duration: {duration}'
-      },
-      {
-        duration: this.durationNode?.textContent
-      }
-    );
+    const tooltip = this.durationNode?.textContent;
     this.setState({
       tooltip
     });
   }
 
   componentDidUpdate() {
-    const { intl } = this.props;
-    const duration = this.durationNode.textContent;
-    const tooltip = intl.formatMessage(
-      {
-        id: 'dashboard.run.duration',
-        defaultMessage: 'Duration: {duration}'
-      },
-      {
-        duration
-      }
-    );
+    const tooltip = this.durationNode?.textContent;
     if (this.state.tooltip !== tooltip) {
       this.setState({ // eslint-disable-line
         tooltip
@@ -77,19 +58,38 @@ class FormattedDurationWrapper extends Component {
   }
 
   render() {
-    const { milliseconds } = this.props;
+    const { intl, milliseconds } = this.props;
     return (
-      <span
-        ref={ref => {
-          this.durationNode = ref;
-        }}
-        title={this.state.tooltip}
-      >
-        <FormattedDuration
-          format="{days} {hours} {minutes} {seconds}"
-          seconds={milliseconds / 1000}
-          unitDisplay="narrow"
-        />
+      <span title={this.state.tooltip}>
+        <span aria-hidden="true">
+          <FormattedDuration
+            format="{days} {hours} {minutes} {seconds}"
+            seconds={milliseconds / 1000}
+            unitDisplay="narrow"
+          />
+        </span>
+        <span
+          className="cds--assistive-text"
+          ref={ref => {
+            this.durationNode = ref;
+          }}
+        >
+          {intl.formatMessage(
+            {
+              id: 'dashboard.run.duration',
+              defaultMessage: 'Duration: {duration}'
+            },
+            {
+              duration: (
+                <FormattedDuration
+                  format="{days} {hours} {minutes} {seconds}"
+                  seconds={milliseconds / 1000}
+                  unitDisplay="long"
+                />
+              )
+            }
+          )}
+        </span>
       </span>
     );
   }
